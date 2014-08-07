88d6b6768101dddbac68b391675c57ce0f3f109d
Chelsea <chelsea_bailey@md-20013613.wgbh.org>
2014-08-07 13:15:48 -0400

    glow -- eventListener rewrite

--- task1/html-glow/js/animate.js
+++ task1/html-glow/js/animate.js
@@ -9,7 +9,16 @@
 		rightLimits = [-200,300],
 		fourLimits = [-650,100],
 		fiveLimits = [-600,600],
-		range=100;
+		range=100,
+		pfx = ['webkit','moz','MS','o',''];
+
+
+	var PrefixedEvent = function(element,type,callback) {
+		for (var p=0; p<pfx.length; p++) {
+			if (!pfx[p]) type = type.toLowerCase();
+				element.addEventListener(pfx[p]+type, callback, false);
+		}
+	}
 
 	var randomizeGlow = function(anim, limits, range) {
 			anim.className = colors[ Math.floor(Math.random() * 5) ];
@@ -25,8 +34,8 @@
 	randomizeGlow(fourAnim, fourLimits, range);
 	randomizeGlow(fiveAnim, fiveLimits, range);
 
-	window.setInterval('randomizeGlow(topAnim, topLimits)',6100) ;
-	window.setInterval('randomizeGlow(leftAnim, leftLimits)',15000) ;
-	window.setInterval('randomizeGlow(rightAnim, rightLimits)',8300) ;
-	window.setInterval('randomizeGlow(fourAnim, fourLimits)',20200) ;
-	window.setInterval('randomizeGlow(fiveAnim, fiveLimits)',12100) ;
+	PrefixedEvent(topAnim, 'AnimationIteration', function(e){randomizeGlow(topAnim, topLimits, range)});
+	PrefixedEvent(leftAnim, 'AnimationIteration', function(e){randomizeGlow(leftAnim, leftLimits, range)});
+	PrefixedEvent(rightAnim, 'AnimationIteration', function(e){randomizeGlow(rightAnim, rightLimits, range)});
+	PrefixedEvent(fourAnim, 'AnimationIteration', function(e){randomizeGlow(fourAnim, fourLimits, range)});
+	PrefixedEvent(fiveAnim, 'AnimationIteration', function(e){randomizeGlow(fiveAnim, fiveLimits, range)});
